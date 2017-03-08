6943cecdd793cc2991f7f0a5eff11e80ba855a52
Nicholas Flynt <thegamefreak0134@gmail.com>
2017-03-08 17:01:50 -0600

    Apply pixel doubling to disassembly panel
    This is the last actual panel that needed a style update.

--- love/panels/disassembler.lua
+++ love/panels/disassembler.lua
@@ -4,21 +4,27 @@ local opcode_names = require("gameboy/opcode_names")
 
 local disassembler = {}
 
-disassembler.width = 256
+disassembler.width = 128 * 2
 
 disassembler.init = function(gameboy)
-  disassembler.canvas = love.graphics.newCanvas(256, 800)
+  disassembler.canvas = love.graphics.newCanvas(128, 400)
   disassembler.gameboy = gameboy
 end
 
 disassembler.draw = function(x, y)
   love.graphics.setCanvas(disassembler.canvas)
   love.graphics.clear()
-  love.graphics.print("Disassembly", 0, 0)
+  love.graphics.setColor(192, 192, 192)
+  love.graphics.rectangle("fill", 0, 0, 128, 400)
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.print("Disassembly", 2, 2)
   disassembler.print_opcodes(disassembler.gameboy)
   love.graphics.setCanvas() -- reset to main FB
   love.graphics.setColor(255, 255, 255)
-  love.graphics.draw(disassembler.canvas, x, y)
+  love.graphics.push()
+  love.graphics.scale(2, 2)
+  love.graphics.draw(disassembler.canvas, x / 2, y / 2)
+  love.graphics.pop()
 end
 
 local opcode_string = function(opcode, data1, data2)
@@ -57,28 +63,30 @@ local opcode_string = function(opcode, data1, data2)
 end
 
 disassembler.print_opcodes = function(gameboy)
-  local y = 30
+  local y = 15
   local pc = gameboy.processor.registers.pc
   local darken_rows = 0
-  for i = 1, math.floor(700 / 20) do
+  love.graphics.setColor(128, 128, 128)
+  love.graphics.rectangle("fill", 0, 14, 128, 7)
+  for i = 1, math.floor((400 - 15) / 7) do
     local name, data_values = opcode_string(gameboy.memory[pc], gameboy.memory[pc + 1], gameboy.memory[pc + 2])
 
     local color = {255, 255, 255}
     if darken_rows > 0 then
-      color = {64, 64, 64}
+      color = {128, 128, 128}
       darken_rows = darken_rows - 1
     else
       if i ~= 1 then
-        color = {192, 192, 192}
+        color = {0, 0, 0}
       end
       if darken_rows == 0 and data_values > 1 then
         darken_rows = data_values - 1
       end
     end
     love.graphics.setColor(unpack(color))
-    love.graphics.print(string.format("%04X: [%02X] %s", pc, gameboy.memory[pc], name), 0, y)
+    love.graphics.print(string.format("%04X: [%02X] %s", pc, gameboy.memory[pc], name), 4, y)
     pc = bit32.band(pc + 1, 0xFFFF)
-    y = y + 20
+    y = y + 7
   end
   love.graphics.setColor(255, 255, 255)
 end
