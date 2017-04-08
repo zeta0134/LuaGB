ded84fe2f62d6913039a83572719f9618934a5d9
Nicholas Flynt <thegamefreak0134@gmail.com>
2017-04-08 16:21:38 -0500

    Ensure that readme and license files (ours and Love2d's) make it into final builds

--- Makefile
+++ Makefile
@@ -2,9 +2,12 @@ love:
 	@mkdir -p love
 	@mkdir -p games
 
-	#cp love/main.lua main.lua
-	zip -9 -r LuaGB.love gameboy games LICENSE.txt
+	zip -9 -r LuaGB.love gameboy games LICENSE.txt README.md
 	cd love && zip -9 -r ../LuaGB.love .
-	#rm main.lua
+
+	-rm LuaGB_Linux.tar.gz
+	cp vendor/love-win32/Love2D_License.txt .
+	tar zcvf LuaGB_Linux.tar.gz LuaGB.love LICENSE.txt README.md Love2D_License.txt
+	-rm Love2D_License.txt
 
 .PHONY : love
--- windows_build.bat
+++ windows_build.bat
@@ -4,18 +4,18 @@ mkdir build
 
 xcopy /S /E /Y /I games love\games
 xcopy /S /E /Y /I gameboy love\gameboy
-copy /Y LICENSE.txt love\LICENSE.txt
 xcopy /S /E /Y /I vendor\love-win32 build
 
-del LuaGB.zip
+del LuaGB_Win32.zip
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('love', 'build\LuaGB.love'); }"
 
 rd /s /q love\games
 rd /s /q love\gameboy
-del love\LICENSE.txt
 
 copy /b build\love.exe+build\LuaGB.love build\LuaGB.exe
+copy /Y LICENSE.txt build\LuaGB_License.txt
+copy /Y README.md build\README.md
 del build\LuaGB.love
 del build\love.exe
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('build', 'LuaGB.zip'); }"
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('build', 'LuaGB_Win32.zip'); }"
 rd /s /q build
