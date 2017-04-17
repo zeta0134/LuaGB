432cd0e72c31672b29e7699c1ce33831662f722b
Nicholas Flynt <thegamefreak0134@gmail.com>
2017-04-17 00:39:47 -0500

    Fix windows build naming
    Anyone noticing a trend? Clearly I am not meant to be a typist.

--- windows_build.bat
+++ windows_build.bat
@@ -6,7 +6,7 @@ xcopy /S /E /Y /I games love\games
 xcopy /S /E /Y /I gameboy love\gameboy
 xcopy /S /E /Y /I vendor\love-win32 build
 
-del LuaGB_Win32.zip
+del LuaGB-Win32.zip
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('love', 'build\LuaGB.love'); }"
 
 rd /s /q love\games
@@ -15,6 +15,7 @@ rd /s /q love\gameboy
 copy /b build\love.exe+build\LuaGB.love build\LuaGB.exe
 copy /Y LICENSE.txt build\LuaGB_License.txt
 copy /Y README.md build\README.md
+copy /Y build\LuaGB.love LuaGB.love
 del build\LuaGB.love
 del build\love.exe
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('build', 'LuaGB-Win32.zip'); }"
