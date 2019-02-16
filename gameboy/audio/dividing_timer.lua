62394b6ba961751f341564fae80dfc8c2ec7ca30
Nicholas Flynt <thegamefreak0134@gmail.com>
2019-02-16 15:21:32 -0600

    Implement setPeriod() and period()

--- gameboy/audio/dividing_timer.lua
+++ gameboy/audio/dividing_timer.lua
@@ -1,10 +1,18 @@
 local DividingTimer = {}
 
 function DividingTimer:new(o)
-   o = o or {}
+   o = o or {_period=0}
    setmetatable(o, self)
    self.__index = self
    return o
+end
+
+function DividingTimer:setPeriod(clocks)
+  self._period = clocks
+end
+
+function DividingTimer:period()
+  return self._period
 end
 
 return DividingTimer
