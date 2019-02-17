31876c2eea9c259be776e69ce43df03bc88c03b5
Nicholas Flynt <thegamefreak0134@gmail.com>
2019-02-16 22:52:06 -0600

    Implement waveform() and setWaveform()

--- gameboy/audio/square_wave_generator.lua
+++ gameboy/audio/square_wave_generator.lua
@@ -1,10 +1,18 @@
 local SquareWaveGenerator = {}
 
 function SquareWaveGenerator:new(o)
-   o = o or {}
+   o = o or {_waveform=0}
    setmetatable(o, self)
    self.__index = self
    return o
+end
+
+function SquareWaveGenerator:waveform()
+  return self._waveform
+end
+
+function SquareWaveGenerator:setWaveform(waveform)
+  self._waveform = waveform
 end
 
 return SquareWaveGenerator
