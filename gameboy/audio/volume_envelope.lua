13891a4183585a56cb4b33841ab5cf758099c8cf
Nicholas Flynt <thegamefreak0134@gmail.com>
2019-02-17 02:42:45 -0600

    Implement volume adjustments when the envelope is clocked

--- gameboy/audio/volume_envelope.lua
+++ gameboy/audio/volume_envelope.lua
@@ -11,4 +11,33 @@ function VolumeEnvelope:new(o)
    return o
 end
 
+function VolumeEnvelope:setVolume(volume)
+  self._volume = volume
+end
+
+function VolumeEnvelope:volume()
+  return self._volume
+end
+
+function VolumeEnvelope:setAdjustment(adjustment)
+  self._adjustment = adjustment
+end
+
+function VolumeEnvelope:adjustment()
+  return self._adjustment
+end
+
+function VolumeEnvelope:clock()
+  if self.timer:period() ~= 8 then
+    local volume = self._volume + self._adjustment
+    if volume > 15 then
+      self._volume = 15
+    elseif volume < 0 then
+      self._volume = 0
+    else
+      self._volume = volume
+    end
+  end
+end
+
 return VolumeEnvelope
