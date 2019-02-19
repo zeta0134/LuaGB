bf4ed0d4f55bbc1722da08a3957f67851e443351
Nicholas Flynt <thegamefreak0134@gmail.com>
2019-02-19 01:41:53 -0600

    Test SquareWaveGenerator's new frequency sweep features

--- tests/audio/square_wave_generator.lua
+++ tests/audio/square_wave_generator.lua
@@ -4,17 +4,17 @@ describe("Audio", function()
       SquareWaveGenerator = require("gameboy/audio/square_wave_generator")
       bit32 = require("bit")
     end)
-    it("can be created", function()
+    before_each(function() 
       square = SquareWaveGenerator:new()
+    end)
+    it("can be created", function()
       assert.are_not_same(square, nil)
     end)
-    it("waveform can be set", function()
-      local square = SquareWaveGenerator:new()
+    it("waveform can be set", function()      
       square:setWaveform(0x0F)
       assert.are_same(square:waveform(), 0x0F)
     end)
     it("clocks adjust the waveform phase 1 bit at a time", function()
-      local square = SquareWaveGenerator:new()
       local test_waveform = 0x2D
       square:setWaveform(test_waveform) -- 00101101
       for i = 1, 8 do
@@ -24,12 +24,37 @@ describe("Audio", function()
       end
     end)
     it("the waveform is clocked when the timer expires", function()
-      local square = SquareWaveGenerator:new()
       square:setWaveform(0x01)
       assert.are_same(square:output(), 1)
       square.timer:reload(10)
       square.timer:advance(10)
       assert.are_same(square:output(), 0)
+    end)
+    it("sweep clocks adjust the period", function()
+      square.frequency_shadow = 500
+      square.sweep_shift = 1
+      square.sweep_negate = false
+      square:sweep()
+      assert.same(square.frequency_shadow, square.period_timer:period())
+      assert.same(square.frequency_shadow, bit32.rshift(500, 1) + 500)
+    end)
+    it("sweep negate adjusts period downwards", function()
+      square.frequency_shadow = 500
+      square.sweep_shift = 1
+      square.sweep_negate = true
+      square:sweep()
+      assert.same(square.frequency_shadow, bit32.bnot(bit32.rshift(500, 1)) + 500)
+    end)
+    it("sweep silences the channel on overflow", function()
+      -- setup tone1 to play a 1 no matter what
+      square:setWaveform(0xFF) -- 00101101
+      -- have the sweep register exceed 2047
+      square.frequency_shadow = 2040
+      square.sweep_shift = 0
+      square.sweep_negate = false
+      square:sweep()
+      -- output should now be 0, indicating the channel has disabled itself
+      assert.same(square:output(), 0)
     end)
   end)
 end)
