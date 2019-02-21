3267d8d2f51841bc8582d05a19c6605174e24e35
Nicholas Flynt <thegamefreak0134@gmail.com>
2019-02-21 11:42:26 -0600

    Correct clock implementation, implement :reset()

--- gameboy/audio/lsfr.lua
+++ gameboy/audio/lsfr.lua
@@ -15,14 +15,19 @@ function LinearFeedbackShiftRegister:new(o)
    return o
 end
 
-function LineraFeedbackShiftRegiser:clock()
+function LinearFeedbackShiftRegister:clock()
   local shift_result = bit32.rshift(self.current_value, 1)
   local xor_result = bit32.band(bit32.bxor(shift_result, self.current_value), 0x1)
   local lfsr_result = bit32.lshift(xor_result, 14) + shift_result
   if self.width_mode == 1 then
-    lfsr_result = bit32.bor(lfsr_result, bit32.lshift(xor_result, 6))
+
+    lfsr_result = bit32.bor(bit32.band(0x7FBF, lfsr_result), bit32.lshift(xor_result, 6))
   end
-  self.current_value = lsfr_result
+  self.current_value = lfsr_result
+end
+
+function LinearFeedbackShiftRegister:reset()
+  self.current_value = 0x7FFF
 end
 
 return LinearFeedbackShiftRegister
