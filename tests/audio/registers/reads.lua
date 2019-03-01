55602e61cf43f5536745b18156ec2e909cff0ee9
Nicholas Flynt <thegamefreak0134@gmail.com>
2019-02-28 19:06:44 -0600

    Expand read test to include region from 0xFF27-0xFF2F

--- tests/audio/registers/reads.lua
+++ tests/audio/registers/reads.lua
@@ -22,19 +22,21 @@ describe("Audio", function()
     it("mock audio module can be created", function()
       assert.not_same(audio, nil)
     end)
-    it("NR10-NR52 read back all 1's for their unused bits, length and frequency counters", function()
+    it("all registers read back all 1's for their unused bits, length and frequency counters", function()
       local read_masks = {
       --NRx0   NRx1   NRx2   NRx3   NRx4
         0x80,  0x3F,  0x00,  0xFF,  0xBF, --NR1x
         0xFF,  0x3F,  0x00,  0xFF,  0xBF, --NR2x
         0x7F,  0xFF,  0x9F,  0xFF,  0xBF, --NR3x
         0xFF,  0xFF,  0x00,  0x00,  0xBF, --NR4x
-        0x00,  0x00,  0x70                --NR5x
+        0x00,  0x00,  0x70,               --NR5x
+        0xFF,  0xFF,  0xFF,  0xFF,  0xFF, --FF27 -  
+        0xFF,  0xFF,  0xFF,  0xFF         --FF2F 
       }
       -- Note: writing 0 to NR52 would interfere with other register reads, since it disables all other
       -- registers, but since we test it last here, there shouldn't be any interference.
-      for address = 0xFF00 + ports.NR10, 0xFF00 + ports.NR52 do
-        local mask = read_masks[address - (0xFF00 + ports.NR10) + 1]
+      for address = 0xFF10, 0xFF2F do
+        local mask = read_masks[address - 0xFF10 + 1]
         memory.write_byte(address, 0x00)
         assert.same(memory.read_byte(address), bit32.bor(0x00, mask))
       end
